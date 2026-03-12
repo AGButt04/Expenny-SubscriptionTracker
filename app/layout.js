e1b0fabc2418b3c182107d1ace2d9a3be8a5ae1d
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-12 15:39:10 -0400

    Added some starting code for the header

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import "./fanta.css"
 import Head from "./Head";
+import Link from "next/link";
+import GoTo from "@/components/Goto";
 
 export const metadata = {
   title: "Expenny · The Subscription Tracker",
@@ -8,11 +10,35 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  const header = (
+    <header>
+      <div>
+        <Link href={'/'}>
+        <h1 className="text-gradient">Expenny</h1>
+        </Link>
+        <p>The Subscription Tracker</p>
+      </div>
+
+      <GoTo />
+    </header>
+  )
+
+  const footer = (
+    <footer>
+      <div className="hard-line" />
+    </footer>
+  )
+
   return (
     <html lang="en">
       <Head />
       <body >
-        {children}
+        {header}
+        <div className="full-line" />
+        <main>
+          {children}
+        </main>
+        {footer}
       </body>
     </html>
   );
