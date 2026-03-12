21caecced92f5543f16079b007b012cfe2455c59
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-12 15:52:29 -0400

    Added some login page logic

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,20 @@
+import Login from "@/components/Login";
+import SubscriptionsDisplay from "@/components/SubscriptionsDisplay";
+import SubscriptionSummary from "@/components/SubsriptionSummary";
+
 export default function DashboardPage() {
+  const isAuthenicated = false;
+
+  if (!isAuthenicated) {
+    return (
+      <Login />
+    )
+  }
+
   return (
-    <div></div>
+    <>
+      <SubscriptionSummary />
+      <SubscriptionsDisplay />
+    </>
   );
 }
