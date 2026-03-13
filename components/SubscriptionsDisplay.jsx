85d9c7c2f02ccc7c508e960f8442446448057a08
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-13 17:04:00 -0400

    Structure the code

--- components/SubscriptionsDisplay.jsx
+++ components/SubscriptionsDisplay.jsx
@@ -14,56 +14,56 @@ export default function SubscriptionsDisplay(props) {
             <h2>Your Subscriptions</h2>
             <div className="card-container">
                 {
-                    userData.subscriptions.map((sub, subIndex) => {
-                        const { name, category, cost, currency, billingFrequency, startDate, notes, status } = sub;
+                userData.subscriptions.map((sub, subIndex) => {
+                    const { name, category, cost, currency, billingFrequency, startDate, notes, status } = sub;
 
-                        return (
-                            <div key={subIndex} className="card subscription-card">
-                                <div>
-                                    <h3>{name}</h3>
-                                    <div className={'status ' + (status === 'Active')? 'card-button-primary' : 'card-button-secondary'}>
-                                        <small>{status}</small>
-                                    </div>
-                                </div>
-
-                                <p><i>{category}</i></p>
-
-                                <div className="sub-cost">
-                                    <h2>${cost}</h2>
-                                    <p>{currency}</p>
-                                </div>
-                                <small>per {billingFrequency}</small>
-
-                                <div className="sub-renewel">
-                                    <div>
-                                        <p>Started</p>
-                                        <h4>{startDate}</h4>
-                                    </div>
-                                    <div>
-                                        <p>Due</p>
-                                        <h4>{getDaysUntilNextCharge(startDate, billingFrequency)}</h4>
-                                    </div>
-                                </div>
-
-                                <div className="white-line" />
-                                <p>{notes}</p>
-
-                                <div className="subscription-actions">
-                                    <button onClick={() => {
-                                        handleDeleteSubscription(subIndex)
-                                    }} className="button-card">
-                                        <i className="fa-solid fa-trash"></i>
-                                        Delete
-                                    </button>
+                    return (
+                        <div key={subIndex} className="card subscription-card">
+                            <div>
+                                <h3>{name}</h3>
+                                <div className={'status ' + (status === 'Active')? 'card-button-primary' : 'card-button-secondary'}>
+                                    <small>{status}</small>
                                 </div>
                             </div>
-                        )
-                    })}
 
-                    <button onClick={handleShowInput} className="button-card add-subscriptions">
-                        <i className="fa-solid fa-plus"></i>
-                        <h5>Add new subscriptions</h5>
-                    </button>
+                            <p><i>{category}</i></p>
+
+                            <div className="sub-cost">
+                                <h2>${cost}</h2>
+                                <p>{currency}</p>
+                            </div>
+                            <small>per {billingFrequency}</small>
+
+                            <div className="sub-renewel">
+                                <div>
+                                    <p>Started</p>
+                                    <h4>{startDate}</h4>
+                                </div>
+                                <div>
+                                    <p>Due</p>
+                                    <h4>{getDaysUntilNextCharge(startDate, billingFrequency)}</h4>
+                                </div>
+                            </div>
+
+                            <div className="white-line" />
+                            <p>{notes}</p>
+
+                            <div className="subscription-actions">
+                                <button onClick={() => {
+                                    handleDeleteSubscription(subIndex)
+                                }} className="button-card">
+                                    <i className="fa-solid fa-trash"></i>
+                                    Delete
+                                </button>
+                            </div>
+                        </div>
+                    )
+                })}
+
+                <button onClick={handleShowInput} className="button-card add-subscriptions">
+                    <i className="fa-solid fa-plus"></i>
+                    <h5>Add new subscriptions</h5>
+                </button>
             </div>
         </section>
     )
