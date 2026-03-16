612e94167dc431e69ed0fd9d64a99c409129a9bf
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-16 15:45:33 -0400

    Added the use state variables for email and password

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,14 +1,29 @@
+'use client'
+
+import { use, useState } from "react"
+
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isRegisteration, setIsRegisteration] = useState('');
+
     return (
         <div className="login">
-            <h2>Login</h2>
-            <input placeholder="Email" type="email" />
-            <input placeholder="password" type="password"/>
+            <h2>{isRegisteration? 'Create an account' : 'Login'}</h2>
+            
+            <input value={email} onChange={(e) => {setEmail(e.target.value)}} 
+            placeholder="Email" type="email" />
+            <input value={password} onChange={(e) => {setPassword(e.target.value)}} 
+            placeholder="password" type="password"/>
+            
             <button>Submit</button>
+            
             <div className="full-line" />
             <div>
-                <p>Don&apos; have an account?</p>
-                <button>Sign Up</button>
+                <p>{isRegisteration? 'Already have an account?' : 'Don\'t have an account?'}</p>
+                <button onClick={() => {
+                    setIsRegisteration(!isRegisteration)
+                }}>{isRegisteration? 'Log in' : 'Sign Up'}</button>
             </div>
         </div>
     )
