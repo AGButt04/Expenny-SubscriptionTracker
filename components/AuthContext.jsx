4030dc65e075579301edd79749ff04245111e0ab
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-17 21:27:04 -0400

    Added the login, logout, and signup functions from firebase

--- components/AuthContext.jsx
+++ components/AuthContext.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { auth, db } from "@/firebase";
-import { onAuthStateChanged } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { doc, getDoc } from "firebase/firestore";
 import { createContext, useContext, useState, useEffect } from "react";
 
@@ -17,6 +17,20 @@ export function AuthProvider(props) {
     const [currentUser, setCurrentUser] = useState(null);
     const [userData, setUserData] = useState();
     const [loading, setLoading] = useState(true);
+
+    function signup(email, password) {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    function login(email, password) {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    function logout() {
+        setCurrentUser(null);
+        setUserData(null);
+        return signOut(auth);
+    }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, async user => {
@@ -51,7 +65,7 @@ export function AuthProvider(props) {
     }, [])
 
     const value = {
-        currentUser, userData, loading
+        currentUser, userData, loading, signup, login, logout
     }
 
     return (
