0f4f21535dcd973cb93252c9e61eec211f8e738f
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-17 22:06:31 -0400

    Added handleEditSubscription and imported the imp data

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,5 +1,6 @@
 'use client'
 
+import { useAuth } from "@/context/AuthContext";
 import Login from "@/components/Login";
 import SubscriptionForm from "@/components/SubscriptionForm";
 import SubscriptionsDisplay from "@/components/SubscriptionsDisplay";
@@ -7,32 +8,43 @@ import SubscriptionSummary from "@/components/SubsriptionSummary";
 import { useState } from "react";
 
 export default function DashboardPage() {
-  const isAuthenicated = false;
+  const isAuthenicated = true;
   const [isAddEntry, setisAddEntry] = useState(false);
+  const { handleDeleteSubscription  } = useAuth();
 
   const [formData, setFormData] = useState({
-        name: '',
-        category: 'Web Services',
-        cost: '',
-        currency: 'USD',
-        billingFrequency: 'Monthly',
-        nextBillingDate: '',
-        paymentMethod: 'Credit Card',
-        startDate: '',
-        renewelType: '',
-        notes: '',
-        status: 'Active'
+      name: '',
+      category: 'Web Services',
+      cost: '',
+      currency: 'USD',
+      billingFrequency: 'Monthly',
+      nextBillingDate: '',
+      paymentMethod: 'Credit Card',
+      startDate: '',
+      renewelType: '',
+      notes: '',
+      status: 'Active'
+  });
+ 
+  function handleChangeInput(event) {
+      const newData = {
+          ...formData,
+          [event.target.name]: event.target.value
+      }
+      setFormData(newData)
+  }
+
+  function handleEditSubscription(index) {
+    const inputObj = userData.subscrioptions.find((val, valIndex) => {
+      return valIndex === index;
     });
 
-    function handleChangeInput(event) {
-        const newData = {
-            ...formData,
-            [event.target.name]: event.target.value
-        }
-        setFormData(newData)
-    }
+    setFormData(inputObj);
+    handleDeleteSubscription(index)
+    setisAddEntry(true);
+  }
 
-  function handleToggleInput() {
+  function handleToggleInput(index) {
     setisAddEntry(!isAddEntry);
   }
 
