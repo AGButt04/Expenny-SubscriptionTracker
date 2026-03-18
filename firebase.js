6e0b996fbdc0b7932ea68352c85562d9cbfdbd1a
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-17 22:05:43 -0400

    Removed the env api variables bug

--- firebase.js
+++ firebase.js
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: NEXT_PUBLIC_FIREBASE_APIKEY,
-  authDomain: NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
-  projectId: NEXT_PUBLIC_FIREBASE_PROJECTID,
-  storageBucket: NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
-  appId: NEXT_PUBLIC_FIREBASE_APPID,
-  measurementId: NEXT_PUBLIC_FIREBASE_MEASUREMENTID
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID
 };
 
 // Initialize Firebase
