e9f256a736ae6ba04404e2cd57f26e2b26086dd8
Abdul Ghani Butt <agbutt@widener.edu>
2026-03-18 21:31:35 -0400

    We are basically done git add .

--- components/Dashboard.jsx
+++ components/Dashboard.jsx
@@ -1,3 +1,5 @@
+// These are the future modifications in in mood.
+
 export default function Dashboard() {
     return (
         <div></div>
--- components/ExpenseCard.jsx
+++ components/ExpenseCard.jsx
@@ -1,3 +1,5 @@
+// These are the future modifications in in mood.
+
 export default function ExpenseCard() {
     return (
         <div></div>
